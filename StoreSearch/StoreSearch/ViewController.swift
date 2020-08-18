46076e3d68abcfc7dab10c6153d32c40d7c03e96
Jonny Baird <jonathan.baird@xdesign.com>
2020-08-18 15:06:55 +0100

    instantiate project

---
+++ StoreSearch/StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Jonathan Baird  on 18/08/2020.
+//  Copyright © 2020 Jonathan Baird . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
